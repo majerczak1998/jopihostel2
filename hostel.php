e4138290823f633cd9c7339e8c4a06f1d56bcba8
majerczak1998 <54272477+majerczak1998@users.noreply.github.com>
2019-09-26 20:06:38 +0200

    Add files via upload

---
+++ hostel.php
@@ -0,0 +1,19 @@
+<?php
+
+$imie = $_POST['imie'];
+$email = $_POST['email'];
+$temat = $_POST['temat'];
+$wiadomosc = $_POST['wiadomosc'];
+
+	echo "$imie";
+
+	echo "$email";
+	
+	echo "$temat";
+	
+	echo "$wiadomosc";
+
+
+
+
+?>
